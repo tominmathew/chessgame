3db771a7266950082ae6b427749d878bcc14366d
tominmathew <tominmathew@outlook.com>
2025-04-25 22:29:44 +0530

    Changed from MVC to reactive

--- chess-game/chess-backend/src/main/java/com/tomin/chess/chess_backend/controller/ChessMoveController.java
+++ chess-game/chess-backend/src/main/java/com/tomin/chess/chess_backend/controller/ChessMoveController.java
@@ -3,27 +3,42 @@ package com.tomin.chess.chess_backend.controller;
 import com.tomin.chess.chess_backend.model.ChessMove;
 import com.tomin.chess.chess_backend.repository.ChessMoveRepository;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/moves")
+@RequiredArgsConstructor
 public class ChessMoveController {
 
     @Autowired
     private ChessMoveRepository repository;
 
     @PostMapping
-    public ChessMove saveMove(@RequestBody ChessMove move){
+    public Mono<ChessMove> saveMove(@RequestBody ChessMove move){
         move.setTimestamp(LocalDateTime.now());
         return repository.save(move);
     }
 
+    @GetMapping
+    public Flux<ChessMove> getAllMoves(){
+        return repository.findAll();
+    }
+
+
     @GetMapping("/{gameId}")
-    public List<ChessMove> getMoves(@PathVariable String gameId){
+    public Flux<ChessMove> getMoves(@PathVariable String gameId){
         return repository.findByGameId(gameId);
+    }
+
+    @DeleteMapping("/{gameId}")
+    public Mono<Void> deleteMove(@PathVariable String gameId){
+        return repository.deleteById(gameId);
     }
 }
--- chess-game/chess-backend/src/main/java/com/tomin/chess/chess_backend/repository/ChessMoveRepository.java
+++ chess-game/chess-backend/src/main/java/com/tomin/chess/chess_backend/repository/ChessMoveRepository.java
@@ -1,10 +1,11 @@
 package com.tomin.chess.chess_backend.repository;
 
 import com.tomin.chess.chess_backend.model.ChessMove;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
-public interface ChessMoveRepository extends MongoRepository<ChessMove, String> {
-    List<ChessMove> findByGameId(String gameId);
+public interface ChessMoveRepository extends ReactiveMongoRepository<ChessMove, String> {
+    Flux<ChessMove> findByGameId(String gameId);
 }
