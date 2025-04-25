560d9c3770b85f3d7f65148521c967128230de30
tominmathew <tominmathew@outlook.com>
2025-04-25 23:42:52 +0530

    Adding kafka into the backend. Changed application.properties to application.yml

---
+++ chess-game/chess-backend/src/main/java/com/tomin/chess/chess_backend/config/kafka/KafkaProducerConfig.java
@@ -0,0 +1,30 @@
+package com.tomin.chess.chess_backend.config.kafka;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaProducerConfig {
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(){
+        return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(producerConfig()));
+    }
+
+    private Map<String, Object> producerConfig(){
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return config;
+    }
+}
